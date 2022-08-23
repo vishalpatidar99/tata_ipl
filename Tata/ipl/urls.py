8fc71cbcb3ccd36762a402a14e98bef32830e307
Vishal Patidar <vishupatidar10@gmail.com>
2022-08-23 20:40:40 +0530

    appjdskv

---
+++ Tata/ipl/urls.py
@@ -0,0 +1,14 @@
+
+from ipl.views import *
+from django.urls import path
+
+
+urlpatterns = [
+    
+    path('',user_list),
+    path('<int:id>',user_details,name='user_details'),
+
+
+   
+
+]
--- Tata/ipl/views.py
+++ Tata/ipl/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+def user_list(request):
+    return render(request,'user_list.html',{'users':User.objects.all()})
+
+def user_details(request,id):
+    return render(request,'user_details.html',{'user ':User.objects.filter(id=id).first()})
